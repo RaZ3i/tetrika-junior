1b16711711d6a8736a37e584776873ebb26c48b4
RaZ3i <tony.ni2016@yandex.ru>
2025-05-31 20:07:31 +0400

    second task complete

--- task_2/solution.py
+++ task_2/solution.py
@@ -1,85 +1,58 @@
-import time
-
+import csv
 from playwright.sync_api import sync_playwright, expect
-import re
 
-animal_arr = []
-char_arr = []
-char_dict = ()
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto(
-        "https://ru.wikipedia.org/w/index.php?title=Категория:Животные_по_алфавиту&pagefrom=Африканские+линзанги#mw"
-        "-pages"
-    )
+def get_file():
+    with sync_playwright() as p:
+        char_dict = {}
+        new_arr = []
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto("https://ru.wikipedia.org/wiki/Категория:Животные_по_алфавиту")
 
-    try:
-        while (
-            expect(
-                page.get_by_role("link")
-                .and_(page.get_by_text("Следующая страница"))
-                .first
-            ).to_be_visible()
-            is None
-        ):
+        try:
+            while (
+                expect(
+                    page.get_by_role("link")
+                    .and_(page.get_by_text("Следующая страница"))
+                    .first
+                ).to_be_visible()
+                is None
+            ):
 
-            all_columns = page.locator("#mw-pages > div.mw-content-ltr")
-            column = all_columns.locator(".mw-category-group").all_inner_texts()
-            count = 0
-            for i in column:
-                temp = i.split(sep="\n")
-                for j in temp:
-                    print(len(j))
-                    if len(j) == 1 and j not in char_arr:
-                        char_arr.append(j)
+                all_columns = page.locator("#mw-pages > div.mw-content-ltr")
+                column = all_columns.locator(".mw-category-group").all_inner_texts()
+
+                for i in column:
+                    temp = i.split(sep="\n")
+                    for j in temp:
+                        new_arr.append(j)
+
+                page.get_by_role("link").and_(
+                    page.get_by_text("Следующая страница")
+                ).first.click(delay=1)
+
+        except Exception:
+            cursor = 1
+            for i in new_arr:
+                if len(i) == 1:
+                    count = 0
+                    for j in range(cursor, len(new_arr)):
+                        if len(new_arr[j]) != 1:
+                            count += 1
+                        else:
+                            cursor = j + 1
+                            break
+                    if char_dict.get(i):
+                        char_dict[i] += count
                     else:
-                        count += 1
-            page.get_by_role("link").and_(
-                page.get_by_text("Следующая страница")
-            ).first.click(delay=1)
-        time.sleep(3)
-        # print(set(char_arr))
-    except Exception:
-        print("БУКАВЫ", char_arr)
-        time.sleep(3)
+                        char_dict.update({i: count})
 
-    # all_char = page.locator(
-    #     "#mw-pages > div.mw-content-ltr > div > div > ul > li"
-    # ).all()
-    # i = 0
-    # for i in all_char:
-    #     char_arr.append(i.text_content()[0])
-    #     print(i.text_content())
+            with open("animals.csv", mode="w", newline="") as file:
+                writer = csv.writer(file)
+                for k, v in char_dict.items():
+                    writer.writerow([f"{k}, {v}"])
 
-    # try:
-    #     while (
-    #         expect(
-    #             page.get_by_role("link")
-    #             .and_(page.get_by_text("Следующая страница"))
-    #             .first
-    #         ).to_be_visible()
-    #         is None
-    #     ):
-    #
-    #         all_char = page.locator(
-    #             "#mw-pages > div.mw-content-ltr > div > div > ul > li"
-    #         ).all_text_contents()[0]
-    #
-    #         for i in all_char:
-    #             j
-    #             if i!= all_char[-1]:
-    #                 if i == i[+1]:
-    #                     char_dict.update({i: })
-    #             char_arr.append(i[0])
-    #
-    #         length = len(char_arr)
-    #
-    #         page.get_by_role("link").and_(
-    #             page.get_by_text("Следующая страница")
-    #         ).first.click(delay=1)
-    #     time.sleep(3)
-    #     print(char_arr)
-    # except Exception:
-    #     time.sleep(3)
+
+if __name__ == "__main__":
+    get_file()
